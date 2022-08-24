99142feafc0d722fba69e98bf14d1adef5ec461c
Carlgo11 <carlgo11@users.noreply.github.com>
2022-08-24 06:01:03 +0200

    Check response status instead of ping

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 const n_max = 2;
 
-async function getHost() {
-    while (true) {
-        const rand = Math.floor(Math.random() * n_max) + 1;
-        const base = `https://api${rand}.tempfiles.download`;
-        if ((await fetch(`${base}/ping/`)).status === 200) return base;
-    }
-}
-
 async function handleRequest(request) {
     const body = await request.formData();
     const url = new URL(request.url);
     const {pathname, search} = url;
-    const base = await getHost();
-    const destinationURL = base + pathname + search;
-    const init = {
-        body: body,
-        method: 'POST'
-    };
-    return await fetch(destinationURL, init);
+    for (let i = 0; i < 10; i++) {
+        const rand = Math.floor(Math.random() * n_max) + 1;
+        const base = `https://api${rand}.tempfiles.download`;
+        const destinationURL = base + pathname + search;
+        const init = {
+            body: body,
+            method: 'POST'
+        };
+        const result = await fetch(destinationURL, init);
+        if (result.status === 201) return result;
+    }
+    return new Response(JSON.stringify({error: "No host available"}), {status: 502})
 }
 
 export default {

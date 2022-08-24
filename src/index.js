7c16e38e8b07124110476e67df7d0fcb4198f55d
Carlgo11 <carlgo11@users.noreply.github.com>
2022-08-24 04:04:01 +0200

    Check if host is up

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 const n_max = 2;
 
-async function sendRequest(url, body) {
-    const init = {
-        body: body,
-        method: 'POST'
-    };
-    const resp = await fetch(url, init);
-    return gatherResponse(resp);
-}
-
-async function gatherResponse(response) {
-    const {headers} = response;
-    const contentType = headers.get('content-type') || '';
-    if (contentType.includes('application/json')) {
-        return JSON.stringify(await response.json());
-    } else if (contentType.includes('application/text')) {
-        return response.text();
-    } else if (contentType.includes('text/html')) {
-        return response.text();
-    } else {
-        return response.text();
+async function getHost() {
+    while (true) {
+        const rand = Math.floor(Math.random() * n_max) + 1;
+        const base = `https://api${rand}.tempfiles.download`;
+        if ((await fetch(`${base}/ping/`)).status === 200) return base;
     }
 }
 
@@ -36,15 +12,13 @@ async function handleRequest(request) {
     const body = await request.formData();
     const url = new URL(request.url);
     const {pathname, search} = url;
-    const rand = Math.floor(Math.random() * n_max) + 1;
-    const base = `https://api${rand}.tempfiles.download`;
+    const base = await getHost();
     const destinationURL = base + pathname + search;
-    return new Response(await sendRequest(destinationURL, body), {
-        headers: {
-            'Access-Control-Request-Method': 'GET,HEAD,POST,DELETE,OPTIONS',
-            'Access-Control-Allow-Origin': '*'
-        }
-    })
+    const init = {
+        body: body,
+        method: 'POST'
+    };
+    return await fetch(destinationURL, init);
 }
 
 export default {

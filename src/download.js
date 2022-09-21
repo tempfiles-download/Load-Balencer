1014aca82264c92e6cf484acecd9bcbc8741b91f
Carlgo11 <carlgo11@users.noreply.github.com>
2022-09-21 05:31:51 +0200

    Use d.* instead of api*.../download/

--- src/download.js
+++ src/download.js
@@ -1,9 +1,17 @@
 export async function download(request) {
-    const {search, searchParams} = new URL(request.url);
+    const {searchParams} = new URL(request.url);
     const id = searchParams.get('id');
+    const password = searchParams.get('p');
     const server = id.charAt(1); // Get server ID from 2nd letter of file ID
-    const base = `https://api${server}.tempfiles.download`;
-    const result = await fetch(base + '/download/' + search);
-    if (result.status === 200) return result;
-    return new Response(JSON.stringify({error: "No host available"}), {status: 502})
+    const api_base = `https://api${server}.tempfiles.download`;
+    const d_base = `https://d${server}.tempfiles.download`;
+    try {
+        if (server === '3') {
+            return await API3.fetch(`${api_base}/${id}/${password}`);
+        } else {
+            return await fetch(`${d_base}/${id}/?p=${password}`);
+        }
+    } catch (e) {
+        return new Request(e.message, {status: e.status})
+    }
 }
